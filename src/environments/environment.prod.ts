6cc81a5c7ac19cd2c9d179cb49afd21a6189db60
dolph <J1enyaG0>
2024-05-22 20:51:46 +0300

    ci\cd(Test) test 16

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAPaVYvKDvnDX0vT5w7m4tbq0puqVCBRLw",
+    authDomain: "mono-blog-63879.firebaseapp.com",
+    databaseURL: "https://mono-blog-63879-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "mono-blog-63879",
+    storageBucket: "mono-blog-63879.appspot.com",
+    messagingSenderId: "1036265756589",
+    appId: "1:1036265756589:web:6f47b68aa9e2707b913324",
+    measurementId: "G-FZZJPL1NEN"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAPaVYvKDvnDX0vT5w7m4tbq0puqVCBRLw",
+    authDomain: "mono-blog-63879.firebaseapp.com",
+    databaseURL: "https://mono-blog-63879-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "mono-blog-63879",
+    storageBucket: "mono-blog-63879.appspot.com",
+    messagingSenderId: "1036265756589",
+    appId: "1:1036265756589:web:6f47b68aa9e2707b913324",
+    measurementId: "G-FZZJPL1NEN"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
