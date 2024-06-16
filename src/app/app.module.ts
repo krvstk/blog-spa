de1428be5f4ce4dd8a44350323d9bcd9a1e390aa
dolph <J1enyaG0>
2024-06-16 18:31:01 +0300

    fix(Spinner): ngx-spinner after update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,7 @@ import { LayoutModule } from './layout/layout.module';
 import { PagenotfoundModule } from '@core/components/pagenotfound/pagenotfound.module';
 import { MatIconModule } from '@angular/material/icon';
 import { HomeComponent } from './main/home/home.component';
-import {AngularFireModule, FIREBASE_OPTIONS} from "@angular/fire/compat";
-import {getAuth, provideAuth} from "@angular/fire/auth";
+import { AngularFireModule, FIREBASE_OPTIONS } from "@angular/fire/compat";
 
 @NgModule({
   declarations: [
